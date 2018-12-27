8fba7d82e0b895f276852c937ab9f6dec6ce4a35
Jessica Trac <jessicamtrac@gmail.com>
2018-12-27 16:44:29 -0500

    added user login and registration functionality

--- application.py
+++ application.py
@@ -1,24 +1,26 @@
 import os, requests
 
 from flask import Flask, session, render_template, request
-# from flask_session import Session
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
-# # Configure session to use filesystem
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
-# # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 
 
 @app.route("/")
@@ -31,32 +33,31 @@ def registration():
 
 @app.route("/mainpage_newregister", methods=['POST'])
 def mainpage_newregister():
-	firstname_new = request.form.get("firstname") # Storing the new user into our database with the relevant information
+	firstname_new = request.form.get("firstname") # Storing the new user into our database
 	lastname_new = request.form.get("lastname")
 	email_new = request.form.get("email")
-	username_new = request.form.get("username")
-	password_new = request.form.get("password")
-	# try:
-	# 	db.execute("INSERT INTO users (username, password) VALUES (:user, :pass)",
-	# 		{"user": username_new, "pass": password_new})
-	# 	db.commit()
-	# 	print("Registered %s to the database" % username_new)
-	# except ValueError:
-	# 	return render_template("error.html", message="Sorry - cannot register user.")
+	pass_new = request.form.get("pass")
+	try:
+		db.execute("INSERT INTO users (first_name, last_name, email, password) VALUES (:first, :last, :email, :pass)",
+			{"first": firstname_new, "last": lastname_new, "email": email_new, "pass": pass_new})
+		db.commit()
+		print("Registered %s to the database" % firstname_new)
+	except ValueError:
+		return render_template("error.html", message="Sorry - cannot register user.")
 	return render_template("mainpage.html")
 
 @app.route("/useraccess", methods=['POST'])
 def useraccess():
-	username_attempt = request.form.get("username")
-	password_attempt = request.form.get("password")
-	# try:
-	# 	user_match = db.execute("SELECT * FROM users WHERE username=:username", {"username": username_attempt}).fetchone()
-	# except ValueError:
-	# 	return render_template("error.html", message="Sorry - we couldn't find you in our system. Please return to try again or register!")
-	# if user_match.password == password_attempt:
-	# 	session['user'] = user_match.username
-	# 	return render_template("search.html", name=session.get('user'))
-	# else:
-	# 	return render_template("error.html", message="Sorry - we couldn't find you in our system. Please return to try again or register!")
+	email_attempt = request.form.get("email")
+	pass_attempt = request.form.get("pass")
+	try:
+		users_match = db.execute("SELECT * FROM users WHERE email=:email", {"email": email_attempt}).fetchone()
+	except ValueError:
+		return render_template("error.html", message="Sorry - we couldn't find you in our system. Please return to try again or register!")
+	if users_match.password == pass_attempt:
+		session['name'] = users_match.first_name
+		return render_template("menu.html", name=session.get('name'))
+	else:
+		return render_template("error.html", message="Sorry - we couldn't find you in our system. Please return to try again or register!")
 	return render_template("mainpage.html")
 
